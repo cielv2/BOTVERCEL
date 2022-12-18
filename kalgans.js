898a09bb7dfb26eebcc518a9192ada31603d5305
cielv2 <117594215+cielv2@users.noreply.github.com>
2022-12-18 12:03:51 +0700

    Update kalgans.js

--- kalgans.js
+++ kalgans.js
@@ -227,7 +227,7 @@ return status}
 //=================================================//
 
 //Kalau Mau Self Lu Buat Jadi False
-haikal.public = true
+haikal.public = false
 //=================================================//
 haikal.serializeM = (m) => smsg(haikal, m, store)
 haikal.ev.on('connection.update', async (update) => {
